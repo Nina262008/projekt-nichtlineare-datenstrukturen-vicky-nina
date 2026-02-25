00d21113c6840252992fb6a09ceafd153caf7ed0
Victoria Beck <victoria.beck@gyho-hattingen.de>
2026-02-25 15:25:11 +0100

    erste Ideen

---
+++ implementierung/Kinoprogramm.java
@@ -0,0 +1,29 @@
+public class Kinoprogramm  {
+  
+  private String name;
+  private int laufzeit;
+  private int preis;
+  private int naechsteVorstellung;
+  private int altersfreigabe;
+  
+  public String getName(){
+    return this.name;
+    }
+  
+  public int getLaufzeit(){
+    return this.laufzeit;
+    }
+    
+  public int getPreis(){
+    return this.Preis;
+    } 
+    
+  public int getNaechsteVorstellung(){
+    return this.naechsteVorstellung;
+    }  
+  
+  public int getAltersfreigabe(){
+    return this.altersfreigabe;
+    }
+  
+  }
