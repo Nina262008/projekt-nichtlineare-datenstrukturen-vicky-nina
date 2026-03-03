eaf3420ed2ccff2d21d6eb7d35c8315bb88bc6aa
Nina <nina@a-v-d-m.com>
2026-03-03 18:13:09 +0100

    Vorlage für die benötigten BSTs eingefügt

--- implementierung/Kinoprogramm.java
+++ implementierung/Kinoprogramm.java
@@ -1,15 +1,36 @@
 public class Kinoprogramm {
 
   public static void traverse(BinarySearchTree<Film> pBst)
+  {
+    if(pBst.getLeftTree().getContent() != null)
     {
-        if(pBst.getLeftTree().getContent() != null)
-        {
-            traverse(pBst.getLeftTree());
-        }
-        System.out.println("Name : " + pBst.getContent().getName() + ", Laufzeit: " + pBst.getContent().getLaufzeit());
-        if(pBst.getRightTree().getContent() != null)
-        {
-            traverse(pBst.getRightTree());
-        }
+      traverse(pBst.getLeftTree());
     }
+    System.out.println("Name : " + pBst.getContent().getName() + ", Laufzeit: " + pBst.getContent().getLaufzeit());
+    if(pBst.getRightTree().getContent() != null)
+    {
+      traverse(pBst.getRightTree());
+    }
+  }
+  
+    public static void Kinoprogramm(String[] args) {
+    BinarySearchTree <Film> BstLaufzeit = new BinarySearchTree <Film> ();
+    BstLaufzeit.insert(new Film (// hier die Attribute des Filmes einfügen));
+    }
+    
+    public static void Kinoprogramm(String [] args) {
+      BinarySearchTree <Film> BstPreis = new BinarySearchTree <Film> ();
+      BstPreis.insert(new Film());
+    }
+    
+    public static void Kinoprogramm(String [] args) {
+      BinarySearchTree <Film> BstNaechsteVorstellung = new BinarySearchTree <Film> ();
+      BstNaechsteVorstellung.insert(new Film());
+    }  
+    
+    public static void Kinoprogramm(String [] args) {
+      BinarySearchTree <Film> BstAltersfreigabe = new BinarySearchTree <Film> ();
+      BstAltersfreigabe.insert(new Film());
+    }
+    
   }
